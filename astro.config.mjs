4584e7a1032192c49772c0c3c0fc908daabce583
chilensis.dev <chilensis.dev@gmail.com>
2025-04-07 14:01:24 +0000

    Setting Astro Project Dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Enable React to support React JSX components.
+  integrations: [
+    react(), 
+    mdx(),
+    sitemap(),
+    partytown({
+      config: { debug: false }
+    })
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,1 @@
+import rss from '@astrojs/rss';
