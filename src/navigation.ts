7a4251be6a64d5f54b212f66a43c62f59d59761e
Chilensis <chilensis.dev@gmail.com>
2025-04-24 00:03:30 +0900

    Add Navigation.ts

---
+++ src/navigation.ts
@@ -0,0 +1,98 @@
+export const headerLinks = {
+    links: [
+        {
+            title: "Company",
+            href: "/about",
+        },
+        {
+            title: "Stories",
+            href: "/stories",
+        },
+        {
+            title: "News",
+            href: "/news",
+        },
+        {
+            title: "Career",
+            href: "/career",
+        },
+    ],
+};
+
+export const footerLinks = {
+    links: [
+        {
+            title: "Products",
+            links: [
+                {
+                    title: "Pelagornis",
+                    href: "/pelagornis",
+                },
+            ],
+        },
+        {
+            title: "Community",
+            links: [
+                {
+                    title: "Slack",
+                    href: "https://discord.gg/astro",
+                },
+                {
+                    title: "GitHub",
+                    href: "https://github.com/pelagornis",
+                },
+                {
+                    title: "Twitter",
+                    href: "https://twitter.com/astrodotbuild",
+                },
+            ],
+        },
+        {
+            title: "Company",
+            links: [
+                {
+                    title: "About us",
+                    href: "/about",
+                },
+                {
+                    title: "Careers",
+                    href: "/careers",
+                },
+                {
+                    title: "Contact",
+                    href: "/contact",
+                },
+            ]
+        },
+        {
+            title: "More",
+            links: [
+                {
+                    title: "News",
+                    href: "/news",
+                },
+                {
+                    title: "Stories",
+                    href: "/stories",
+                },
+            ],
+        },
+        {
+            title: "Terms & Policies",
+            links: [
+                {
+                    title: "Terms of Use",
+                    href: "/terms-of-use",
+                },
+                {
+                    title: "Privacy Policy",
+                    href: "/privacy",
+                },
+                {
+                    title: "Security",
+                    href: "/security",
+                },
+            ],
+        },
+    ],
+};
